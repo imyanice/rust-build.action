8683208d035de525e1aba0ea32f1059ff1d4afc2
Yanice <git@yanice.me>
2024-05-02 08:30:06 +0200

    added target field

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,11 @@
 import { execa } from 'execa'
 import fs from 'node:fs'
 import * as core from '@actions/core'
+import { getInput } from '@actions/core'
 
 try {
   execa('echo', ['args'], { stdio: 'inherit' })
+  console.log(getInput("targets"))
   console.log(fs.readdirSync('./'))
 } catch (error) {
   // Fail the workflow run if an error occurs
